6b0d0efd787c753a4092d7d05b6be8efcfbdf0b1
minikaz98 <42753827+minikaz98@users.noreply.github.com>
2018-09-26 14:58:04 -0400

    Add files via upload
    helloworldvariables homework

---
+++ HelloWorldVariables.java
@@ -0,0 +1,13 @@
+
+package	edu.yu.intro;
+
+public class HelloWorldVariables {
+	public static void main (final String [] args) {
+		int s1 = 3;
+		double s2 = 2.52;
+		System.out.println("Hello World!");
+		System.out.println("The world stands on " + s1 + " pillars");
+		System.out.println("Not " +  s2 + " pillars");
+		System.out.println("and ... this is a very true statement");
+    }   
+}
