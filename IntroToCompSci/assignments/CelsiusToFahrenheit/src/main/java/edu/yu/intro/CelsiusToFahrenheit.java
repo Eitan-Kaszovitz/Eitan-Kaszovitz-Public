dc1b70ea45ece51149150eac25613478c2c2e6f3
minikaz98 <42753827+minikaz98@users.noreply.github.com>
2018-10-10 17:01:06 -0400

    Update CelsiusToFahrenheit.java

--- IntroToCompSci/assignments/CelsiusToFahrenheit/src/main/java/edu/yu/intro/CelsiusToFahrenheit.java
+++ IntroToCompSci/assignments/CelsiusToFahrenheit/src/main/java/edu/yu/intro/CelsiusToFahrenheit.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class CelsiusToFahrenheit {
 	public static void main (final String [] args) {
 		Scanner input = new Scanner (System.in);
-		System.out.println("Enter temperature in Celsius: ");
+		System.out.print("Enter temperature in Celsius: ");
 		float c = input.nextFloat();
 		float f = 32 + (c * 9/5.0f); 
 		System.out.printf("%.1f C = %.1f F ", c, f);
