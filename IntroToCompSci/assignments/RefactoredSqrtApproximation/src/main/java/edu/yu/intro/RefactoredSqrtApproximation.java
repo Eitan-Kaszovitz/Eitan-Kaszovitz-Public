4222b3423b337510574eb5bc0b4ffea88962045e
eitankaszovitz <ekaszovi@mail.yu.edu>
2018-11-05 18:08:03 -0500

    editing RSA

--- IntroToCompSci/assignments/RefactoredSqrtApproximation/src/main/java/edu/yu/intro/RefactoredSqrtApproximation.java
+++ IntroToCompSci/assignments/RefactoredSqrtApproximation/src/main/java/edu/yu/intro/RefactoredSqrtApproximation.java
@@ -4,35 +4,28 @@ package	edu.yu.intro;
 import java.util.Scanner;
 import java.io.*; 
 import java.util.*;
-import java.lang.NegativeArraySizeException;
-import java.util.InputMismatchException;
+import java.lang.IllegalArgumentException;
 
 public class RefactoredSqrtApproximation {
 	public static void main (final String [] args) {
 		Scanner input = new Scanner (System.in);
 		System.out.print("Enter ‘n‘ -- I will calculate all square roots from 0 to n (inclusive) using Babylonian approximation algorithm: ");
-		try {
-			int number = input.nextInt();
-			double [] bigArray = calculateSquareRoots(number + 1);
-			double c = 0;
-			System.out.println("***********************************************");
-			for (int x = 0; x < (bigArray.length); x++) {
-				c = bigArray[x];
-				System.out.printf("sqrt(%d) = %.3f%n", x, c);
-			}
+		int number = input.nextInt();
+		if (number < 0) {
+			throw new IllegalArgumentException();
+		}
+		double [] bigArray = calculateSquareRoots(number + 1);
+		double c = 0;
 		System.out.println("***********************************************");
+		for (int x = 0; x < (bigArray.length); x++) {
+			c = bigArray[x];
+			System.out.printf("sqrt(%d) = %.3f%n", x, c);
 		}
-		catch (NegativeArraySizeException e){
-			System.err.println("error: input can't be negative");
-			System.exit(0);
-		}
-		catch (InputMismatchException e){
-			System.err.println("error: input must be positive integer");
-			System.exit(0);
-		}
+		System.out.println("***********************************************");
+		
 	}
 	
-	public static double [] calculateSquareRoots (int n) {
+	public static double[] calculateSquareRoots (int n) {
 		int number = n;
 		double[] sqrtArray = new double [number];
 		for (int i = 0; i < number; i++){
@@ -48,11 +41,6 @@ public class RefactoredSqrtApproximation {
 		double FirstGuess = number / 2;
 		double NextGuess = number / 2.1;
 		
-		/*if (a <= .001) {
-			System.out.println("error: input must be positive and above relative zero");
-			System.exit(0);
-		}*/
-		
 		while (Math.abs(FirstGuess - NextGuess) >= 0.0001) {
 			FirstGuess = NextGuess;
 			double b = (number / FirstGuess);
@@ -60,7 +48,4 @@ public class RefactoredSqrtApproximation {
 		}
 		return NextGuess;
 	}
-
-
-	
 }
