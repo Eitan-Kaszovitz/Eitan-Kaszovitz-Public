407a8722e5a1916a2ee15e61c1702f1b59e99a8b
eitankaszovitz <ekaszovi@mail.yu.edu>
2018-11-12 17:53:42 -0500

    editing SAT

--- IntroToCompSci/assignments/JUnit4SqrtApproximation/src/test/java/edu/yu/intro/test/SqrtApproximationTest.java
+++ IntroToCompSci/assignments/JUnit4SqrtApproximation/src/test/java/edu/yu/intro/test/SqrtApproximationTest.java
@@ -37,15 +37,19 @@ public class SqrtApproximationTest {
 		assertEquals("botched input of 10", 3.16227766017 , JUnit4SqrtApproximation.sqrt(10), .001);
 	}
 	
+	double[] expectedOutput1 = {0};
+	double[] methodOutput1 = JUnit4SqrtApproximation.calculateSquareRoots(0);
 	@Test 
 	public void calculateSquareRootsMinimalLength() {
-		JUnit4SqrtApproximation.calculateSquareRoots(0);
+		assertArrayEquals("botched input of 0", expectedOutput1, methodOutput1, .001);
 	}
 
+	double[] expectedOutput2 = {0, 1, 1.4142, 1.73205, 2};
+	double[] methodOutput2 = JUnit4SqrtApproximation.calculateSquareRoots(4);
+	
 	@Test 
 	public void calculateSquareRootsHappyPaths() {
-		JUnit4SqrtApproximation.calculateSquareRoots(4);
+		assertArrayEquals("botched input of 4", expectedOutput2, methodOutput2, .001);
 	}
 }
-
 
